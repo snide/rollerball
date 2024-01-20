fb4994233ec5b2f1b6bf2e2fb71747a625fc3519
Dave Snider <dave.snider@gmail.com>
2024-01-20 11:22:10 -0500

    getall for default, add proper sort order

--- fetch.ts
+++ fetch.ts
@@ -7,14 +7,20 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
 const xata = new XataClient({ apiKey: process.env.XATA_API_KEY, branch: 'main' });
 
 const main = async () => {
-  const searchPhrase: string = process.argv[2] || '';
+  const searchPhrase: string | undefined = process.argv[2] || undefined;
 
   try {
-    const results = await xata.db.links.search(searchPhrase, {
-      fuzziness: 1
-    });
+    let records;
+    if (!searchPhrase) {
+      records = await xata.db.links.sort('xata.createdAt', 'desc').getAll();
+    } else {
+      const results = await xata.db.links.search(searchPhrase, {
+        fuzziness: 1
+      });
+      records = results.records;
+    }
 
-    const formattedResults = results.records
+    const formattedResults = records
       .map((record: LinksRecord) => {
         const trimmedUrl = record.url.substring(0, 90); // URL
         const trimmedTitle = record.title ? record.title.substring(0, 45) : 'Untitled'; // Title, default to "Untitled" if empty
